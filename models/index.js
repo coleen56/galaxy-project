c9d05f75e94b43484eafb97c748e94b09fbe98dd
lino <jlino@greta-bretagne-sud.fr>
2026-02-17 13:36:25 +0100

    database relation done

--- models/index.js
+++ models/index.js
@@ -30,6 +30,9 @@ db.user = require("../models/user.model.js")(sequelize, DataTypes);
 db.role = require("../models/role.model.js")(sequelize, DataTypes);
 db.refresh_token = require("../models/refresh_token.model")(sequelize, DataTypes);
 db.trip = require("../models/trip.model.js")(sequelize, DataTypes);
+db.planet = require("../models/planet.model.js")(sequelize, DataTypes);
+db.planetarySystem = require("../models/planetarySystem.model.js")(sequelize, DataTypes);
+db.galaxy = require("../models/galaxy.model.js")(sequelize, DataTypes);
 
 // many to many user <=> roles
 db.user.belongsToMany(db.role, {
@@ -70,7 +73,50 @@ db.trip.belongsToMany(db.user, {
 });
 
 // one to many trajet <=> planète de départ
+db.trip.hasOne(db.planet, {
+    foreignKey: "departure_planet_id",
+    as: 'departure_planet'
+})
 
+// one to many trajet <=> planète d'arrivée'
+db.trip.hasOne(db.planet, {
+    foreignKey: "arrival_planet_id",
+    as: 'arrival_planet'
+})
+
+// many to one planète départ <=> trajet
+db.planet.hasMany(db.trip, {
+    foreignKey: "departure_planet_id",
+    as: "departing_trips"
+})
+
+// many to one planète arrivée <=> trajet
+db.planet.hasMany(db.trip, {
+    foreignKey: "arrival_planet_id",
+    as: "arriving_trips"
+})
+
+// one to many planetarySystem <=> planet
+db.planetarySystem.hasMany(db.planet, {
+    foreignKey: "planetary_system_id",
+    as: "planets"
+})
+
+db.planet.belongsTo(db.planetarySystem, {
+    foreignKey: "planetary_system_id",
+    as: "planetary_system"
+})
+
+// one to many galaxy <=> planetarySystem
+db.galaxy.hasMany(db.planetarySystem, {
+    foreignKey: "galaxy_id",
+    as: "planetary_systems"
+})
+
+db.planetarySystem.belongsTo(db.galaxy, {
+    foreignKey: "galaxy_id",
+    as: "galaxy"
+})
 
 // liste des rôles possibles
 db.ROLES = ["user", "admin", "moderator"];
